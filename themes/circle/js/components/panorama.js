e4a3e3ab21606ec1f1298aa57dda80bd9b2069d7
Chris <social@krisdigital.com>
2017-02-10 20:59:03 +0100

    Panorama

--- themes/circle/js/components/panorama.js
+++ themes/circle/js/components/panorama.js
@@ -1,10 +1,12 @@
-import Pannellum from 'pannellum'
+import pannellum from 'pannellum'
 import $ from 'jquery'
 
-// $('[data-panorama-url]').each((index, el) => {
-//   panorama.viewer(el, {
-//     type: 'equirectangular',
-//     panorama: $(el).data('panorama-url')
-//   })
-// })
-console.log(Pannellum)
+$('[data-panorama-url]').each((index, el) => {
+  pannellum.viewer(el, {
+    type: 'equirectangular',
+    panorama: $(el).data('panorama-url'),
+    mouseZoom: false,
+    showControls: false,
+    preview: decodeURI($(el).data('panorama-preview-url'))
+  })
+})
