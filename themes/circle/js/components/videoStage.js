76611645e3785db4bb2cccbdf301f2a8234c0b0d
Chris <social@krisdigital.com>
2017-02-16 13:39:39 +0100

    video stage js

--- themes/circle/js/components/videoStage.js
+++ themes/circle/js/components/videoStage.js
@@ -1,17 +1,42 @@
 import Player from '@vimeo/player'
+import {classanimateIn, classanimateOut} from './classanimate'
 import $ from 'jquery'
 
 $('[data-video-stage]').each((index, el) => {
+  let currentId = 0
+  
+  const activateThumbWithId = (id) => {
+    const $thumb = $(`[data-vimeo-video-id=${id}]`, $(el))
+    classanimateOut($('[data-vimeo-video-id]', $(el)))
+    classanimateIn($thumb)
+    return $thumb
+  }
+  
+  const getIdFromThumb = ($thumb) => {
+    return $thumb.data('vimeo-video-id')
+  }
+  
   const player = new Player($('[data-video-player]', $(el))[0], {
-    id: parseInt($(el).find('[data-vimeo-video-id]').first().data('vimeo-video-id'))
+    id: getIdFromThumb($(el).find('[data-vimeo-video-id]').first())
+  })
+  
+  player.on('loaded', (e) => {
+    currentId = e.id
+    activateThumbWithId(e.id)
   })
 
   $('[data-vimeo-video-id]', $(el)).on('click', (e) => {
-    player.loadVideo( parseInt( $(e.currentTarget).data('vimeo-video-id') ) ).then((id) => {
-      console.log(id)
+    const lastId = currentId
+    const newId = getIdFromThumb( $(e.currentTarget) )
+
+    if (lastId != newId) {
+      player.loadVideo( newId ).then((id) => {
+        player.play()
+      }).catch((e) => {
+        console.log(e);
+      })
+    } else {
       player.play()
-    }).catch((e) => {
-      console.log(e);
-    })
+    }
   })
 })
