58879145013ee098509716636cb6fd7414064428
Chris <social@krisdigital.com>
2017-02-21 00:13:25 +0100

    Circle Areas hierarchy contact module paragraph

--- themes/circle/js/components/circleSlider.js
+++ themes/circle/js/components/circleSlider.js
@@ -14,13 +14,22 @@ const setActive = (currentSlide) => {
 //       $(el).addClass('active-circle')
 //     }
 //   })
-  $('[data-circle-details]').html($slider.find('.slick-center .details__text').html()).animate({opacity: 1}, 500)
+  $('[data-circle-details]').html($slider.find('.slick-center .details__text').html()).animate({opacity: 1}, 300)
 }
-$slider.slick()
+$slider.slick({
+  responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3
+        }
+      }
+    ]
+})
 $slider.on('beforeChange', (event, slick, currentSlide, nextSlide) => {
   // $slider.find('.slide').removeClass('active-circle move-left move-right')
   // setActive(nextSlide)
-  $('[data-circle-details]').animate({opacity: 0}, 500)
+  $('[data-circle-details]').animate({opacity: 0}, 300)
 })
 $slider.on('afterChange', (event, slick, currentSlide) => {
   setActive(currentSlide)
