be4d78f20688f75bc0a70bfb6861c5b772ffc2cc
Chris <social@krisdigital.com>
2017-02-23 23:40:48 +0100

    floor and house tables

--- themes/circle/js/pages/house.js
+++ themes/circle/js/pages/house.js
@@ -3,8 +3,15 @@ import $ from 'jquery'
 
 $('[data-svg-url]').each((index, el) => {
   const snap = Snap(el)
+  const syncBuildingHeight = () => {
+    $(el).find('svg').height($('.house-floors__table').height())
+  }
   Snap.load($(el).data('svg-url'), (fragment) => {
     snap.append(fragment)
     $('[id^=Highlight]').hide()
+    $(window).on('resize.houseHeight', () => {
+      syncBuildingHeight()
+    })
+    syncBuildingHeight()
   })
 })
