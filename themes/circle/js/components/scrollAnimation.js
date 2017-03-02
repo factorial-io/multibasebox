46e239aa413f26da5cf43b11e46e1959481ac981
Chris <social@krisdigital.com>
2017-03-02 22:48:59 +0100

    contact scroll animation

--- themes/circle/js/components/scrollAnimation.js
+++ themes/circle/js/components/scrollAnimation.js
@@ -2,6 +2,7 @@ import lazyload from '../utils/lazyload'
 import $ from 'jquery'
 import {classanimateOut, classanimateIn} from '../components/classanimate'
 import ScrollSpy, {SpyDirection, SpyState} from '../components/ScrollSpy'
+import TweenLite from 'gsap/TweenLite'
 
 const sel = '.image-with-text:visible h2, .image-with-text:visible .para, .image-with-text:visible .button, .image-with-text:visible img, .news-teaser .article-teaser'
 classanimateOut($(`${sel}`))
@@ -11,3 +12,15 @@ spy.onIn(($el) => {
   lazyload($el)
   classanimateIn($el)
 })
+
+if ($('[data-sold-percentage]')[0]) {
+  const percentageSpy = new ScrollSpy($('[data-sold-percentage]'), SpyDirection.bottom)
+  const tweenObj = {percentage: 0}
+  percentageSpy.onIn(($el) => {
+    const valueMax = $el.attr('aria-valuemax')
+    TweenLite.to(tweenObj, 2, {percentage: $el.data('sold-percentage'), onUpdate: () => {
+      $el.css({width: `${tweenObj.percentage}%`})
+      $el.find('[data-sold-value]').html(Math.round(valueMax * tweenObj.percentage / 100))
+    }})
+  })
+}
