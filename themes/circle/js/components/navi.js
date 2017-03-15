69ed9c589a6d536549e7d7da15fa13a0078be71e
Chris <social@krisdigital.com>
2017-03-15 17:14:16 +0100

    scroll nav to active element

--- themes/circle/js/components/navi.js
+++ themes/circle/js/components/navi.js
@@ -6,3 +6,16 @@ $(document).on('click', '[data-burger]', (e) => {
     $('[data-flyout-navi]').append([$('#block-mainmenuen').clone(), $('#block-headermeta').clone()])
   }
 })
+
+let $el = $('#block-mainmenu')[0] ? $('#block-mainmenu') : $('.area-navigation')
+if ($el[0]) {
+  setTimeout(() => {
+    const $active = $el.find('.is-active')
+    if ($active.offset().left + $active.outerWidth() > $el.width()) {
+      const maxScroll = $el[0].scrollWidth - $el.width()
+      $el.animate({
+          scrollLeft: Math.min(maxScroll, $active.offset().left + $active.outerWidth() - $el.width())
+      }, 500)
+    }
+  }, 800)
+}
