424eee85ea38666ac0a56b47dcc4658f32d82d7c
Chris <social@krisdigital.com>
2017-03-16 11:31:09 +0100

    floor no interior

--- themes/circle/js/pages/floor.js
+++ themes/circle/js/pages/floor.js
@@ -1,14 +1,13 @@
 import $ from 'jquery'
 import lazyload from '../utils/lazyload'
 
-if ($('.page-node-type-rental-floor')[0]) {
-  const $normalImg = $('.area-stage__images img').first()
+if ($('.page-node-type-rental-floor')[0] && $('.area-stage__images img').length > 1) {
   const $interiorImg = $('.area-stage__images img').last()
   const getState = () => {
    return $interiorImg.is(':visible') 
   }
   $interiorImg.hide()
-  
+
   $('[data-interior-switch]').on('click', (e) => {
     if (getState()) {
       $interiorImg.hide()
