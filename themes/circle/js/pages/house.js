4451c8facf6982f5620953eb09c4e074f760e7bf
Yang Wang <wangyang118@gmail.com>
2017-07-03 18:26:46 +0200

    SP-88: clean

--- themes/circle/js/pages/house.js
+++ themes/circle/js/pages/house.js
@@ -40,8 +40,9 @@ if ($('.page-node-type-rental-house')[0]) {
       }).on('mouseout', (e) => {
         $(`[id^="Highlight_"]`).removeClass('active')
       }).on('click', (e) => {
-        if ($(e.currentTarget).find('a').length) {
-          window.location.href = $(e.currentTarget).find('a').attr('href');
+        const link = $(e.currentTarget).find('a');
+        if (link.length) {
+          window.location.href = link.attr('href');
         }
       })
     })
