42251bb29230ed4bdf107f337fc06f2625a4d32e
Yang Wang <wangyang118@gmail.com>
2017-07-10 10:28:14 +0200

    SP-95: Add swiper modul

---
+++ themes/circle/js/components/downloadSlider.js
@@ -0,0 +1,4 @@
+import $ from 'jquery'
+import Swiper from 'swiper';
+
+console.log('downloadSlider');
--- themes/circle/js/pages/all.js
+++ themes/circle/js/pages/all.js
@@ -11,5 +11,6 @@ import '../components/map'
 import '../components/totop'
 import '../components/video'
 import '../components/contact'
+import '../components/downloadSlider'
 
 import '../utils/ieFixes'
